1f1062accd3943a5a7e3d9d82cab1ff3f9ab66e7
Pablo4V4 <81110075+Pablo4V4@users.noreply.github.com>
2021-03-30 20:38:03 +0200

    Add files via upload

---
+++ Techlabs:13:Happines:Robbin/assets/js/main.js
@@ -0,0 +1,366 @@
+/**
+* Template Name: Flexor - v2.4.1
+* Template URL: https://bootstrapmade.com/flexor-free-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  var scrolltoOffset = $('#header').outerHeight() - 1;
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top - scrolltoOffset;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, .mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+
+  // Mobile Navigation
+  if ($('.nav-menu').length) {
+    var $mobile_nav = $('.nav-menu').clone().prop({
+      class: 'mobile-nav d-lg-none'
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').append('<div class="mobile-nav-overly"></div>');
+
+    $(document).on('click', '.mobile-nav-toggle', function(e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-overly').toggle();
+    });
+
+    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+      e.preventDefault();
+      $(this).next().slideToggle(300);
+      $(this).parent().toggleClass('active');
+    });
+
+    $(document).click(function(e) {
+      var container = $(".mobile-nav, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-overly').fadeOut();
+        }
+      }
+    });
+  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+    $(".mobile-nav, .mobile-nav-toggle").hide();
+  }
+
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Stick the header at top on scroll
+  $("#header").sticky({
+    topSpacing: 0,
+    zIndex: '50'
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+  // Initiate the venobox plugin
+  $(window).on('load', function() {
+    $('.venobox').venobox();
+  });
+
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Playlist carousel (uses the Owl Carousel library)
+  $(".playlist-carousel").owlCarousel({
+    autoplay: true,
+    center: true,
+    dots: true,
+    loop: true,
+    items: 2,
+  });
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
+
+// Chatbot
+
+const trigger = [
+  //0 
+  ["hi", "hey", "hello"],
+  //1
+  ["how are you", "how are things"],
+  //2
+  ["what is going on", "what is up"],
+  //3
+  ["happy", "good", "well", "fantastic", "cool"],
+  //4
+  ["bad", "bored", "tired", "sad"],
+  //5
+  ["tell me a story", "tell me a joke"],
+  //6
+  ["thanks", "thank you"],
+  //7
+  ["bye", "good bye", "goodbye"]
+];
+
+const reply = [
+  //0 
+  ["Hello!", "Hi!", "Hey!", "Hi there!"], 
+  //1
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  //2
+  [
+    "Nothing much",
+    "Exciting things!"
+  ],
+  //3
+  ["Glad to hear it"],
+  //4
+  ["Why?", "Cheer up buddy"],
+  //5
+  ["What about?", "Once upon a time..."],
+  //6
+  ["You're welcome", "No problem"],
+  //7
+  ["Goodbye", "See you later"]
+];
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Try again",
+  "I'm listening...",
+  "Bro..."
+];
+
+
+
+document.addEventListener("DOMContentLoaded", function(eventLoaded) {
+  const inputField = document.getElementById("input")
+  
+  inputField.addEventListener("keydown", function(eventKeyDown) {
+    if (eventKeyDown.code === "Enter") {
+        let input = inputField.value;
+        inputField.value = "";
+        output(input);
+
+        var objDiv = document.getElementById("chat");
+        objDiv.scrollTop = objDiv.scrollHeight;
+      }
+    }
+  );
+
+
+function filterText (input) {
+  //remove all characters except word characters, space, and digits
+  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+
+  // 'tell me a story' -> 'tell me story'
+  // 'i feel happy' -> 'happy'
+  text = text
+    .replace(/ a /g, " ")
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "");
+
+  return text;
+}
+
+function compare(triggerArray, replyArray, text) {
+  let item;
+  for (let x = 0; x < triggerArray.length; x++) {
+    for (let y = 0; y < replyArray.length; y++) {
+      if (triggerArray[x][y] == text) {
+        items = replyArray[x];
+        item = items[Math.floor(Math.random() * items.length)];
+      }
+    }
+  }
+  return item;
+}
+
+function output(input) {
+  let product;
+
+  input = filterText(input);
+  
+  if (compare(trigger, reply, input)) {
+    product = compare(trigger, reply, input);
+  } else if (input.match(/robot/gi)) {
+    product = robot[Math.floor(Math.random() * robot.length)];
+  } else {
+    product = alternative[Math.floor(Math.random() * alternative.length)];
+  }
+
+  //update DOM
+  addChat(input, product);
+}
+
+function addChat(input, product) {
+  const mainDiv = document.getElementById("chat");
+  let userDiv = document.createElement("div");
+  userDiv.className = "user";
+  userDiv.innerHTML = `You:<span class="user-response">${input}</span>`;
+  mainDiv.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  botDiv.className = "bot";
+  botDiv.innerHTML = `<span class="bot-response">${product}</span> :Chatbot `;
+  mainDiv.appendChild(botDiv);
+}
+
+
+})
